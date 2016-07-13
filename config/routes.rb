062d09d18982bfe60d206d876cc180df6b35b18a
warrenbrown <warren@katapultmedia.com>
2016-07-13 07:10:17 -0500

    Added todo list and todo items

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'todo_items/create'
+
+  resources :todo_lists do
+    resources :todo_items
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'todo_lists#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
